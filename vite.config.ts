47316b73d391ca15b538e8757f6631db636a2096
dev.roychung96 <dev.roychung96@gmail.com>
2024-12-02 01:25:56 +0800

    chore: fix url

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/react-dnd-kit-tailwind-shadcn-ui/" : "/",
+  base: "/",
 }));
